849a07ec559fd6e6d164cd8a135a57750e732187
Jpveeravalli <veeravallijayaprakash@gmail.com>
2024-09-01 21:33:55 +0100

    EF core with database context

---
+++ Entities/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities.Models;
+
+public partial class User
+{
+    public byte Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string EmailId { get; set; } = null!;
+
+    public byte[] Password { get; set; } = null!;
+}
--- UserLogin/Program.cs
+++ UserLogin/Program.cs
@@ -1,11 +1,12 @@
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<>((options) =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//})
+builder.Services.AddDbContext<UserManagementContext>((options) =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 app.UseStaticFiles();
